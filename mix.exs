a2eb9b6a0fe872f55eb6cc67d88a727b84affa4a
Maksym Kolodnytskyi <mcsym28+macos@gmail.com>
2024-05-19 17:45:31 +0300

    Move generating functions to lib_dev

--- mix.exs
+++ mix.exs
@@ -7,9 +7,13 @@ defmodule OpenAPIGenerator.MixProject do
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env())
     ]
   end
+
+  defp elixirc_paths(env) when env in ~w(dev test)a, do: ["lib", "lib_dev"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help compile.app" to learn about applications.
   def application do
